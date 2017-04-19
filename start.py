f5b95d8526aa033b9a4151131ec3929356f47c24
Unknown <igoshaaaaaa@gmail.com>
2017-04-19 05:48:30 +0300

    Source files

---
+++ start.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+import logging
+
+from server import ChatServer
+from client import ChatClient
+
+print("dasdas")
+logging.basicConfig(level=logging.DEBUG)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--client', action='store_true')
+parser.add_argument('--server', action='store_true')
+parser.add_argument('-p', '--port', type=int, default=8077)
+parser.add_argument('-s', '--addr', type=str, default='0.0.0.0')
+args = parser.parse_args()
+
+if args.client:
+	ChatClient(host=args.addr, port=args.port).start()
+elif args.server:
+	ChatServer(host=args.addr, port=args.port).start()
