a2411382bce41b1e99598f035d06fb636cb4e728
lgAshanl <igoshaaaaaa@gmail.com>
2017-05-20 06:21:37 +0300

    Clear code

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import collections
 import socket
-import select, sys, re
+import select
+import sys
+import re
 from logging import info as log_info
 import datetime
 from time import time
@@ -32,13 +34,14 @@ class ChatServer(object):
         self.connected_clients = []
         self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            self.db = psycopg2.connect("dbname='chat_db' user='postgres' host='localhost' password='***'")
+            self.db = psycopg2.connect(
+                "dbname='chat_db' user='postgres' host='localhost' password='***'")
             log_info("Connected to {dbname} as {user}, host: {host}".format(
                 dbname='chat_db',
                 user='postgres',
                 host='localhost'
             ))
-        except:
+        except BaseException:
             log_info("I am unable to connect to the database")
 
     def _register_client(self, client):
@@ -51,7 +54,10 @@ class ChatServer(object):
         client.sock.close()
 
     def _get_client_by_sock(self, sock):
-        clients = list(filter(lambda x: x.sock == sock, self.connected_clients))
+        clients = list(
+            filter(
+                lambda x: x.sock == sock,
+                self.connected_clients))
         assert len(clients) == 1
         return clients[0]
 
@@ -76,7 +82,8 @@ class ChatServer(object):
             socks_to_read = list(map(lambda x: x.sock, self.connected_clients))
 
             # Linux epoll magic
-            socks_ready_to_read, _, _ = select.select([self.server_sock] + socks_to_read, [], [])
+            socks_ready_to_read, _, _ = select.select(
+                [self.server_sock] + socks_to_read, [], [])
 
             for sock in socks_ready_to_read:
                 if sock == self.server_sock:
@@ -90,94 +97,93 @@ class ChatServer(object):
                     data = recv_until_end_messages(client.sock)
                     if not data:
                         self._unregister_and_close_client(client)
-                        log_info("{} is offline (initiated by the client)".format(str(client)))
+                        log_info(
+                            "{} is offline (initiated by the client)".format(
+                                str(client)))
                         continue
 
-                    log_info("recieved {len} bytes from {client}: {data}".format(
-                        len=len(data),
-                        client=client,
-                        data=data
-                    ))
+                    log_info(
+                        "recieved {len} bytes from {client}: {data}".format(
+                            len=len(data), client=client, data=data))
 
                     request = ChatRequest()
                     request.ParseFromString(data)
-                    if request.command_type == ChatRequest.BROADCAST_MSG:
-                        print("message=", request.message)
-                        if request.message == "":
-                            request.message = "test_message"
-                        self._send_broadcast_message(request.message)
-                    elif request.command_type == ChatRequest.GET_CLIENTS:
-                        self._send_broadcast_message(str(self.connected_clients).encode())
-                    elif request.command_type == ChatRequest.SIGN_UP:
-                        response = ChatResponse()
-                        response.command_type = ChatResponse.SIGN_UP
-                        if self._is_user_in_db(request.sign.login):
-                            response.successful = False
-                            response.message = "This user already exist"
-                        else:
-                            if request.sign.passwd and request.sign.login:
-                                self._add_user_to_db(request.sign.login, request.sign.passwd)
-                                response.successful = True
-                                response.message = "You registered"
-                            else:
-                                response.successful = False
-                                response.message = "Insert password and login, bastard"
-
-                        self._send_message_to_client(client, response.SerializeToString())
-
-                        log_info("SING_UP:{} is {} (initiated by the client)\n\t\t{}".format(
-                            str(client),
-                            str(response.successful),
-                            response.message
-                        ))
-                    elif request.command_type == ChatRequest.SIGN_IN:
-                        response = ChatResponse()
-                        response.command_type = ChatResponse.SIGN_IN
-                        if self._is_user_in_db(request.sign.login)\
-                                and self._login_user(client, request.sign.login, request.sign.passwd, request.sign.hidden):
-                            response.successful = True
-                            response.message = "You logged"
-                        else:
-                            response.successful = False
-                            response.message = "This user dose not already exist or wrong login/password"
-
-                        self._send_message_to_client(client, response.SerializeToString())
-
-                        log_info("SING_IN:{} is {} (initiated by the client)\n\t\t{}".format(
-                            str(client),
-                            str(response.successful),
-                            response.message
-                        ))
-                    elif client.id is not None:
-                        if request.command_type == ChatRequest.ADD_CHAT:
-                            self._add_chat(client, request.info_text)
-                        elif request.command_type == ChatRequest.ADD_USERS_TO_CHAT:
-                            self._add_users_to_chat(client, request.info_text)
-                        elif request.command_type == ChatRequest.GET_CHATS_AND_MESSAGES:
-                            response = ChatResponse()
-                            response.command_type = ChatResponse.CHATS_AND_MESSAGES
-                            self._get_chats_to_response(client, response.chats)
-                            if request.info_text != '' and request.info_text.isdigit():
-                                limit = int(request.info_text)
-                            else:
-                                limit = 10
-                            self._get_messages_to_response(response.messages, limit, client_id=client.id)
-                            response.successful = True
-                            self._send_message_to_client(client, response.SerializeToString())
-                        elif request.command_type == ChatRequest.MSG:
-                            self._broadcast_msg(client, request.message)
-                        elif request.command_type == ChatRequest.DELETE_MSG:
-                            self._delete_message(client, int(request.info_text))
-                        else:
-                            log_info("shit")
+                    if self._input_for_logging(client, request):
+                        continue
+                    if client.id is not None:
+                        self._input_with_logged_user(client, request)
                     elif client.id is None:
-                        # self._get_messages_by_chats([])
-                        log_info("Client {client}: ohuel".format(
+                        log_info("Client {client}: did something wrong".format(
                             client=client,
                         ))
                     else:
                         log_info("shit")
                         exit(0)
+
+    def _input_for_logging(self, client, request):
+        if request.command_type == ChatRequest.SIGN_UP:
+            response = ChatResponse()
+            response.command_type = ChatResponse.SIGN_UP
+            if self._is_user_in_db(request.sign.login):
+                response.successful = False
+                response.message = "This user already exist"
+            else:
+                if request.sign.passwd and request.sign.login:
+                    self._add_user_to_db(
+                        request.sign.login, request.sign.passwd)
+                    response.successful = True
+                    response.message = "You registered"
+                else:
+                    response.successful = False
+                    response.message = "Insert password and login, bastard"
+
+            self._send_message_to_client(client, response.SerializeToString())
+
+            return True
+        elif request.command_type == ChatRequest.SIGN_IN:
+            response = ChatResponse()
+            response.command_type = ChatResponse.SIGN_IN
+            if self._is_user_in_db(
+                    request.sign.login) and self._login_user(
+                    client,
+                    request.sign.login,
+                    request.sign.passwd,
+                    request.sign.hidden):
+                response.successful = True
+                response.message = "You logged"
+            else:
+                response.successful = False
+                response.message = "This user dose not already exist or wrong login/password"
+
+            self._send_message_to_client(client, response.SerializeToString())
+
+            return True
+        else:
+            return False
+
+    def _input_with_logged_user(self, client, request):
+        if request.command_type == ChatRequest.ADD_CHAT:
+            self._add_chat(client, request.info_text)
+        elif request.command_type == ChatRequest.ADD_USERS_TO_CHAT:
+            self._add_users_to_chat(client, request.info_text)
+        elif request.command_type == ChatRequest.GET_CHATS_AND_MESSAGES:
+            response = ChatResponse()
+            response.command_type = ChatResponse.CHATS_AND_MESSAGES
+            self._get_chats_to_response(client, response.chats)
+            if request.info_text != '' and request.info_text.isdigit():
+                limit = int(request.info_text)
+            else:
+                limit = 10
+            self._get_messages_to_response(
+                response.messages, limit, client_id=client.id)
+            response.successful = True
+            self._send_message_to_client(client, response.SerializeToString())
+        elif request.command_type == ChatRequest.MSG:
+            self._broadcast_msg(client, request.message)
+        elif request.command_type == ChatRequest.DELETE_MSG:
+            self._delete_message(client, int(request.info_text))
+        else:
+            log_info("shit")
 
     def _is_user_in_db(self, login):
         cursor = self.db.cursor()
@@ -196,7 +202,8 @@ class ChatServer(object):
     def _login_user(self, client, login, passwd, hidden):
         # UPDATE products SET price = 10 WHERE price = 5;
         cursor = self.db.cursor()
-        cursor.execute("SELECT * FROM users WHERE name = %s and password = %s;", (login, passwd,))
+        cursor.execute(
+            "SELECT * FROM users WHERE name = %s and password = %s;", (login, passwd,))
         rows = cursor.fetchall()
 
         if not bool(len(rows)):
@@ -269,9 +276,9 @@ class ChatServer(object):
                 if len(rows) == 0:
                     continue
                 user_id = rows[0][0]
-                cursor.execute("INSERT INTO chat_users (user_id, chat_id, hidden) "
-                               "VALUES (%s, %s, %s);",
-                               (user_id, chat_id, False))
+                cursor.execute(
+                    "INSERT INTO chat_users (user_id, chat_id, hidden) "
+                    "VALUES (%s, %s, %s);", (user_id, chat_id, False))
                 self._send_chat(user_id, chat_id)
             self.db.commit()
 
@@ -332,20 +339,20 @@ class ChatServer(object):
 
     def _get_messages_by_chat(self, chat_id, limit):
         cursor = self.db.cursor()
-        cursor.execute("SELECT 	messages.message_id, chats.chat_id AS chat, users.name AS author, "
-                       "        time, tag, text, answer_id, file_id "
-                       "FROM chats "
-                       "INNER JOIN messages "
-                       "    ON chats.chat_id = messages.chat_id "
-                       "INNER JOIN users "
-                       "    ON users.user_id = messages.from_id "
-                       "WHERE chats.chat_id = %s "
-                       "ORDER BY time DESC "
-                       "LIMIT %s;",
-                       (chat_id, limit,))
+        cursor.execute(
+            "SELECT 	messages.message_id, chats.chat_id AS chat, users.name AS author, "
+            "        time, tag, text, answer_id, file_id "
+            "FROM chats "
+            "INNER JOIN messages "
+            "    ON chats.chat_id = messages.chat_id "
+            "INNER JOIN users "
+            "    ON users.user_id = messages.from_id "
+            "WHERE chats.chat_id = %s "
+            "ORDER BY time DESC "
+            "LIMIT %s;", (chat_id, limit,))
         rows = cursor.fetchall()
         true_rows = []
-        for i in range(len(rows)-1, 0, -1):
+        for i in range(len(rows) - 1, 0, -1):
             true_rows.append(rows[i])
         return true_rows
 
@@ -379,7 +386,12 @@ class ChatServer(object):
             return message_id
         return -1
 
-    def _get_messages_to_response(self, response_messages, limit, client_id=0, chats=None):
+    def _get_messages_to_response(
+            self,
+            response_messages,
+            limit,
+            client_id=0,
+            chats=None):
         deleted_msg = set()
         if chats is None:
             chats = self._get_chats_by_id(client_id)
@@ -405,9 +417,9 @@ class ChatServer(object):
     def _is_user_in_chat(self, user_id, chat_id):
         cursor = self.db.cursor()
         cursor.execute("SELECT user_id FROM chat_users "
-                        "WHERE user_id = %s AND chat_id = %s "
-                        "LIMIT 1;",
-                        (user_id, chat_id,))
+                       "WHERE user_id = %s AND chat_id = %s "
+                       "LIMIT 1;",
+                       (user_id, chat_id,))
         rows = cursor.fetchall()
         return bool(len(rows))
 
@@ -453,7 +465,8 @@ class ChatServer(object):
         mes.ip = client.addr[0]
         mes.from_id = client.id
         mes.from_name = rows[0][0]
-        mes.time = datetime.datetime.fromtimestamp(time()).strftime('%Y-%m-%d %H:%M:%S')
+        mes.time = datetime.datetime.fromtimestamp(
+            time()).strftime('%Y-%m-%d %H:%M:%S')
         mes.tag = 'message'
         mes.text = request.text
         if request.answer_id == 0:
@@ -496,10 +509,10 @@ class ChatServer(object):
         cursor.execute("INSERT INTO messages (chat_id, ip, from_id, "
                        "time, tag, text, answer_id, file_id) "
                        "VALUES (%s, %s, %s, %s, %s, %s, %s, %s);",
-                        (message.chat_id, message.ip, message.from_id,
-                         message.time, message.tag, message.text,
-                         None,
-                         message.file_id,))
+                       (message.chat_id, message.ip, message.from_id,
+                        message.time, message.tag, message.text,
+                        None,
+                        message.file_id,))
         self.db.commit()
         cursor.execute("SELECT lastval();")
         rows = cursor.fetchall()
@@ -557,7 +570,8 @@ class ChatServer(object):
         rows = cursor.fetchall()
         chat_name = rows[0][0]
         response = self._preparing_chat_for_response(chat_id, chat_name)
-        self._get_messages_to_response(response.messages, 10, chats=[[chat_id]])
+        self._get_messages_to_response(
+            response.messages, 10, chats=[[chat_id]])
         response = response.SerializeToString()
         for client in self.connected_clients:
             if client.id == user_id:
